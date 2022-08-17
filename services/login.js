c782abb245efb4a0bc08cb7950a7cfe6add7da24
ssrG12 <santiagosrueda@gmail.com>
2022-08-16 20:44:44 -0500

    Test Conection

--- services/login.js
+++ services/login.js
@@ -4,14 +4,14 @@ import { environment } from '../environments/environment'
 export const loginService = () => {
 
   const login = async (data) => {
-    const response = await axios.post(`${environment.url_api}authApplicationDrops`, data, {
+    const response = await axios.post(`${environment.url_api}authApplicationBelcorp`, data, {
       headers: {
         "Access-Control-Allow-Headers": "Content-Type",
         "Access-Control-Allow-Origin": "https://www.example.com",
         "Access-Control-Allow-Methods": "POST,GET"
       }
     })
-    console.log(response)
+    return response
   }
 
   return {
