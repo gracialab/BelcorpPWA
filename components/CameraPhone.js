46fdd16b33e27b319a4fb1f5f06db5e207ba0cb5
Roiner Villarreal <devcoderv@gmail.com>
2022-09-28 12:33:54 -0400

    imagen para la version movil en la camara

--- components/CameraPhone.js
+++ components/CameraPhone.js
@@ -9,72 +9,79 @@ import { photosProvider } from '../providers/photos/photosProvider'
 export const CameraPhone = () => {
   const { capture, capture2, capture3, imageOne, imageTwo, imageThree, canvasRef, handleVideoOnPlay, modelsLoaded, videoRef,outputCanvas } = photosProvider()
   return (
-    <div className={styles.container}>
-      <HeaderConfig title={'Fotos'} />
-      <div className={styles.adviceHeader} >
-        <div className={styles.advise} >
-          <div className={styles.alertWeb} >
-            <Image src={alert} width={30} height={30} />
-          </div>
-          <p className={styles.adviseMessage} >
-            Coloca tu rostro {!imageOne ? 'de frente' : !imageTwo ? 'de lado izquierdo' : !imageThree ? 'de lado derecho' : ''} en el círculo y toma la foto.
-          </p>
-          <div className={styles.alert} >
-            <Image src={camera} width={50} height={50} />
-          </div>
-        </div>
-        <div className={styles.backCamera} >
-          <div className={styles.circleWrapper}>
-            <div className={styles.circleContainer}>
-              <div className={styles.circlePhoto}>
-                <video ref={videoRef} className={styles.video} onPlay={() => handleVideoOnPlay()} />
-                <canvas ref={outputCanvas} className={styles.videoOutput}></canvas>
-                {modelsLoaded ?
-                  <div className={styles.canva} >
-                    <canvas ref={canvasRef} />
-                  </div> : <></>
-                }
-              </div>
+    <div>
+      <div className={styles.container}>
+        <HeaderConfig title={'Fotos'} />
+        <div className={styles.adviceHeader} >
+          <div className={styles.advise} >
+            <div className={styles.alertWeb} >
+              <Image src={alert} width={30} height={30} />
+            </div>
+            <p className={styles.adviseMessage} >
+              Coloca tu rostro {!imageOne ? 'de frente' : !imageTwo ? 'de lado izquierdo' : !imageThree ? 'de lado derecho' : ''} en el círculo y toma la foto.
+            </p>
+            <div className={styles.alert} >
+              <Image src={camera} width={50} height={50} />
             </div>
           </div>
-          <div className={styles.rowSteps} >
-            {imageOne ?
-              <div className={styles.done} >
-                <Image src={check} />
-              </div> :
-              <div className={styles.empty} />
-            }
-            <div className={styles.line} />
-            {imageTwo ?
-              <div className={styles.done} >
-                <Image src={check} />
-              </div> :
-              <div className={styles.empty} />
-            }
-            <div className={styles.line} />
-            {imageThree ?
-              <div className={styles.done} >
-                <Image src={check} />
-              </div> :
-              <div className={styles.empty} />
-            }
+          <div className={styles.backCamera} >
+            <div className={styles.circleWrapper}>
+              <div className={styles.circleContainer}>
+                <div className={styles.circlePhoto}>
+                  <div className={styles.circlePhotoContent}>
+                    <video ref={videoRef} className={styles.video} onPlay={() => handleVideoOnPlay()} />
+                    <div>
+                      <canvas ref={outputCanvas} className={styles.videoOutput}></canvas>
+                    </div>
+                    {modelsLoaded ?
+                      <div className={styles.canva} >
+                        <canvas ref={canvasRef} />
+                      </div> : <></>
+                    }
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.rowSteps} >
+              {imageOne ?
+                <div className={styles.done} >
+                  <Image src={check} />
+                </div> :
+                <div className={styles.empty} />
+              }
+              <div className={styles.line} />
+              {imageTwo ?
+                <div className={styles.done} >
+                  <Image src={check} />
+                </div> :
+                <div className={styles.empty} />
+              }
+              <div className={styles.line} />
+              {imageThree ?
+                <div className={styles.done} >
+                  <Image src={check} />
+                </div> :
+                <div className={styles.empty} />
+              }
+            </div>
+            <div className={styles.rowSteps} >
+              <p className={styles.subtitle} >
+                Frontal
+              </p>
+              <p className={styles.subtitle} >
+                Lado izq
+              </p>
+              <p className={styles.subtitle} >
+                Lado der
+              </p>
+            </div>
+            <button className={styles.submit} onClick={!imageOne ? () => capture() : !imageTwo ? () => capture2() : !imageThree ? () => capture3() : null} >
+              Tomar foto
+            </button>
           </div>
-          <div className={styles.rowSteps} >
-            <p className={styles.subtitle} >
-              Frontal
-            </p>
-            <p className={styles.subtitle} >
-              Lado izq
-            </p>
-            <p className={styles.subtitle} >
-              Lado der
-            </p>
-          </div>
-          <button className={styles.submit} onClick={!imageOne ? () => capture() : !imageTwo ? () => capture2() : !imageThree ? () => capture3() : null} >
-            Tomar foto
-          </button>
         </div>
-      </div>
-    </div >
+        <div className={styles.space} />
+      </div >
+    </div>
   )
 }
